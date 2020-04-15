a81df586281d9246631e68e33696690777af4406
Sagi Amar <sagitimamar@gmail.com>
2020-04-16 01:53:54 +0300

    need to work on index cards

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,30 @@
+class CocktailsController < ApplicationController
+  
+
+  def index
+    @cocktails = Cocktail.all
+
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/edit'
+  get 'cocktails/create'
+  get 'cocktails/update'
+  get 'cocktails/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  resources :cocktails do
+  	resources :doses
+  	resources :reviews
+  end
+
+
+
+
+
+
 end
